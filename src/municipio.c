608943f92c7f1ee9fddb190a147fc29f94fb8e89
unknown <rafael.tsutomu@ufms.br>
2023-05-21 19:06:27 -0400

    feat: finalizando interface 1

--- src/municipio.c
+++ src/municipio.c
@@ -5,13 +5,16 @@
 #include "funcao.h"
 
 int main(){
+	vizinho *neighbors;
 	tnode *node;
 	muni temp;
 	FILE *arq;
+	point aux;
+	int size;
 	int option; //option_menu;
 
 	do{
-		printf("Digite a opção:\n0 - Sair\n1 - Procurar pelos municípios mais próximos do Brasil\n2 - Procurar pelos fastfoods mais próximos\n");
+		printf("0 - Sair\n1 - Procurar pelos municípios mais próximos do Brasil\n2 - Procurar pelos fastfoods mais próximos\nDigite a opção: ");
 		scanf("%d",&option);
 
 		if(option == 1){
@@ -35,6 +38,28 @@ int main(){
 					temp.fuso_horario), 0);
 				}
 			}
+			printf("\nDigite quantos vizinhos mais próximos deseja buscar:\n");
+			scanf("%d", &size);
+			printf("\nDigite as coordenadas: \n");
+			scanf("%lf %lf", &aux.coord[0], &aux.coord[1]);
+
+			neighbors = malloc(size * sizeof(vizinho));
+			initialize_neighbors(neighbors, size);
+
+			searchNeighbors(node, aux, neighbors, size, 0);
+
+			for(int i = 0; i<size; i++){
+				muni *municipios = (muni *)neighbors[i].vizin;
+				printf("%d° cidade mais perto:\n", i+1);
+				printf("Código IBGE: %d\nNome do município: %s\nLatitude: %f\nLongitude: %f\nCapital: %d\nCódigo UF: %d\nSiafi ID: %s\nDDD: %d\nFuso horário: %s\nRegião: %s\nUF: %s\n\n", 
+					municipios->cod_ibge, municipios->nome, municipios->coord[0], municipios->coord[1], 
+					municipios->capital, municipios->codigo_uf, municipios->siafi_id, municipios->ddd,
+					municipios->fuso_horario, municipios->regiao, municipios->uf);
+			}
+			printf("\n\n");
+
+			free(neighbors);
+			freeNode(node);
 		}
 
 		/*else if(option == 2){
